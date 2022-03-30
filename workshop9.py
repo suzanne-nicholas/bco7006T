651bf1a0dbda4db5cbbad69e33fa24be9c4b73ec
Suzanne Nicholas <suzmnicholas@gmail.com>
2022-03-30 21:03:56 +1100

    numpy

---
+++ workshop9.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.array([[1,2,3],
+             [4,5,6],
+             [7,8,9]])
+print(a.shape)
+
+a = np.array([1,2,3,4,5,6])
+b = np.array([1,2,3,4,5])
+c = a + b
+print(c)
+
+print(c.shape)
+
+1 = [[1,2,3], [3,6,9], [2,4,6]] # create a list
+a = np.array
